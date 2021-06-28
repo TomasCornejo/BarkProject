41e6801646347077f20d2abb1cfcb6d5eec45c79
TomasCornejo <tcornejo@fen.uchile.cl>
2021-06-28 15:03:53 -0400

    changed args

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ import flask
 from app import app
 from app.models import DataRow
 from app import db, app
-from flask import abort
+from flask import abort, request
 import os
 
 
@@ -16,12 +16,12 @@ def index():
 @app.route('/input', methods=['POST'])
 def inputreg():
     try:
-        json_data = flask.request.json
-        if json_data['key_input'] == os.environ['INPUTKEY']:
-            row = DataRow(datetimeRegister=json_data['datetime'], value=json_data['value'])
+        data = request.args
+        if data.get('key_input') == os.environ['INPUTKEY']:
+            row = DataRow(datetimeRegister=data.get('datetime'), value=data.get('value'))
             db.session.add(row)
             db.session.commit()
-            return "Inserted Row: {datetime}//{value}".format(datetime=json_data['datetime'], value=json_data['value'])
+            return "Inserted Row: {datetime}//{value}".format(datetime=data.get('datetime'), value=data.get('value'))
     except TypeError:
         return "Not Today"
 
