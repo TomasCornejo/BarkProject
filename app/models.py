5567828084518016d71f1fbc678bdcd5409ab73c
TomasCornejo <tcornejo@fen.uchile.cl>
2021-06-28 01:33:12 -0400

    cambio de idea

--- app/models.py
+++ app/models.py
@@ -1,6 +1,4 @@
 from app import db
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 
 
 class DataRow(db.Model):
@@ -10,18 +8,3 @@ class DataRow(db.Model):
 
     def __repr__(self):
         return '<{dTime}: {value}>'.format(dTime=self.datetimeRegister, value=self.value)
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,10 @@
+import flask
+
 from app import app
-from flask_login import current_user, login_user, login_required, logout_user
-from app.models import User, DataRow
+from app.models import DataRow
 from app import db
-from flask import abort, request
+from flask import abort
 import os
-
-
-@app.route('/login/<username>&<password>', methods=['GET', 'POST'])
-def login(username, password):
-    if current_user.is_authenticated:
-        return 'Already In!!'
-    user = User.query.filter_by(username=username).first()
-    if user is None or user.check_password(password) or request.remote_addr != os.environ['IPADRESSADMIN']:
-        return 'Not Today!!'
-    login_user(user, remember=True)
-    return 'Lets Get it!'
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return 'Go Away Man!'
 
 
 @app.route('/')
@@ -28,12 +12,12 @@ def index():
     return f"Hello, Hello"
 
 
-@app.route('/input&<datetime>&<value>')
-@login_required
-def input(datetime, value):
-    if request.remote_addr == os.environ['IPADRESSADMIN']:
-        row = DataRow(datetimeRegister=datetime, value=value)
+@app.route('/input', methods=['GET', 'POST'])
+def inputreg():
+    json_data = flask.request.json
+    if json_data['key_input'] == os.environ['INPUTKEY']:
+        row = DataRow(datetimeRegister=json_data['datetime'], value=json_data['value'])
         db.session.add(row)
         db.session.commit()
-        return "Inserted Row: {datetime}//{value}".format(datetime=datetime, value=value)
+        return "Inserted Row: {datetime}//{value}".format(datetime=json_data['datetime'], value=json_data['value'])
     return abort(403)
